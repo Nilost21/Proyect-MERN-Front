82671fb42f71b5bc77ed9a79166872347375391d
Nicolas Molina <nicolasmolina0920@hotmail.com>
2024-08-25 23:03:04 -0300

    Fixing an error in the label

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -3,9 +3,9 @@ import { Outlet } from 'react-router-dom';
 const AuthLayout = () => {
   return (
     <>
-      <min className="container mx-auto md:grid md:grid-cols-2 mt-12">
+      <main className="container mx-auto md:grid md:grid-cols-2 mt-12">
         <Outlet />
-      </min>
+      </main>
     </>
   );
 };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,7 @@ const Login = () => {
     <>
       <div>
         <h1 className="text-indigo-600 font-black text-8xl">
-          Log in and manage your <span className="text-white">patients</span>
+          Log in and manage your <span className="text-white">Patients</span>
         </h1>
       </div>
       <div>2</div>
